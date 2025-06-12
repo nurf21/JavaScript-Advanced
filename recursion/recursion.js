884650e2164f0fba9f34753e8bfbbab3b348e5b4
nurf21 <nurfauzangiri@gmail.com>
2025-06-12 13:24:02 +0700

    Added factorial function

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -20,3 +20,17 @@ function sumToFormula(n) {
 console.log(sumToLoop(100));
 console.log(sumToRecursive(100));
 console.log(sumToFormula(100));
+
+// Calculate factorial
+// ==================================================
+
+function factorial(n) {
+  // Base case: factorial of 0 or 1 is 1.
+  if (n <= 1) {
+    return 1;
+  }
+  // Recursive call: n! = n * (n - 1)!
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
