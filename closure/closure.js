2d75ce4faa9fc54fa28d9a92cfbba28abc882c66
nurf21 <nurfauzangiri@gmail.com>
2025-06-13 13:51:36 +0700

    Added new functions and code to closure.js

--- closure/closure.js
+++ closure/closure.js
@@ -53,3 +53,25 @@ console.log(users);
 // Sorting by age:
 users.sort(byField('age'));
 console.log(users);
+
+// Army of functions
+// ==================================================
+
+function makeArmy() {
+  let shooters = [];
+
+  for (let i = 0; i < 10; i++) {
+    let shooter = function () {
+      console.log(i);
+    };
+    shooters.push(shooter);
+  }
+
+  return shooters;
+}
+
+let army = makeArmy();
+
+army[0]();
+army[1]();
+army[2]();
