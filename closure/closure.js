2621efa6fa3d3043c521ca5f00dfaadc94ff15d5
nurf21 <nurfauzangiri@gmail.com>
2025-06-13 13:42:26 +0700

    Added byField function with example usage

--- closure/closure.js
+++ closure/closure.js
@@ -31,3 +31,25 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(arr.filter(inBetween(3, 6)));
 console.log(arr.filter(inArray([1, 2, 10])));
+
+// Sort by field
+// ==================================================
+
+function byField(field) {
+  return (a, b) => a[field] > b[field] ? 1 : -1;
+}
+
+// Example usage:
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+// Sorting by name:
+users.sort(byField('name'));
+console.log(users);
+
+// Sorting by age:
+users.sort(byField('age'));
+console.log(users);
