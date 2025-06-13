fcef69e83c967ef959978a74a9bb9baebe108d5a
nurf21 <nurfauzangiri@gmail.com>
2025-06-13 13:39:41 +0700

    Added filter functions inBetween and inArray with example usage.

--- closure/closure.js
+++ closure/closure.js
@@ -10,3 +10,24 @@ function sum(a) {
 // Example usage:
 console.log(sum(1)(2));
 console.log(sum(5)(-1));
+
+// Filter through function
+// ==================================================
+
+function inBetween(a, b) {
+  return function (x) {
+    return x >= a && x <= b;
+  };
+}
+
+function inArray(arr) {
+  return function (x) {
+    return arr.includes(x);
+  };
+}
+
+// Example usage:
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr.filter(inBetween(3, 6)));
+console.log(arr.filter(inArray([1, 2, 10])));
