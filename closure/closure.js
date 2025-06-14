42fb14fac1b7e0c4b81eb83cb98ecfd8688cbdfc
nurf21 <nurfauzangiri@gmail.com>
2025-06-14 12:06:29 +0700

    Refactor sum function from traditional closure to arrow function syntax

--- closure/closure.js
+++ closure/closure.js
@@ -1,11 +1,7 @@
 // Sum with closures
 // ==================================================
 
-function sum(a) {
-  return function (b) {
-    return a + b;
-  };
-}
+const sum = a => b => a + b;
 
 // Example usage:
 console.log(sum(1)(2));
