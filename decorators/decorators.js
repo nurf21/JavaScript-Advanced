1a7d9c0aa88aff375e25c4d67655f740a21e000b
nurf21 <nurfauzangiri@gmail.com>
2025-06-16 11:00:31 +0700

    Added delay decorator implementation and example usage

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -22,3 +22,25 @@ work(4, 5);
 for (let args of work.calls) {
   console.log('call: ' + args.join(','));
 }
+
+// Delaying decorator
+// ==================================================
+
+function delay(f, ms) {
+  return function (...args) {
+    const context = this;
+    setTimeout(() => {
+      f.apply(context, args);
+    }, ms);
+  };
+}
+
+function f(x) {
+  console.log(x);
+}
+
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+
+f1000("test");
+f1500("test");
